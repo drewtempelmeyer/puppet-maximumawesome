61b0076b488dd880497cbebb52b3d765bed4c78f
Drew Tempelmeyer <drewtemp@gmail.com>
2013-09-14 16:34:02 -0700

    Start working on maximumawesome

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,14 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class maximumawesome {
+
+  repository {
+    source   => 'square/maximum-awesome',
+    provider => 'git',
+    path     => "${boxen::config::srcdir}/maximumawesome"
+  }
+
+  exec { "rake":
+    command => "cd ${boxen::config::srcdir}/maximumawesome && rake",
+    creates => "${boxen::config::srcdir}/.vim"
+  }
+
 }
--- spec/classes/maximumawesome_spec.rb
+++ spec/classes/maximumawesome_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 # Rename this file to classname_spec.rb
 # Check other boxen modules for examples
 # or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+describe 'maximumawesome' do
   it do
     should contain_anchor('Hello_World')
   end
